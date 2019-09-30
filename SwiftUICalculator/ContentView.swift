7caaafa26f061fe77d27b0a407cbd5d7b3355ad4
Seanlinsanity <seanlin1016@gmail.com>
2019-09-30 15:57:08 +0800

    Initial Commit

---
+++ SwiftUICalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICalculator
+//
+//  Created by Sean on 2019/9/30.
+//  Copyright © 2019 Sean. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
