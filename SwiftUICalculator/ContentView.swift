2603f03ba29c4308fa5aced32e7053f4cd00be59
Seanlinsanity <seanlin1016@gmail.com>
2019-09-30 17:51:51 +0800

    build layuout in Swift UI

--- SwiftUICalculator/ContentView.swift
+++ SwiftUICalculator/ContentView.swift
@@ -8,14 +8,85 @@
 
 import SwiftUI
 
-struct ContentView: View {
+extension Color {
+    static let lightGray = Color(red: 0.6, green: 0.6, blue: 0.6)
+    static let darkGray = Color(red: 0.2, green: 0.2, blue: 0.2)
+}
+
+struct CalculatorButton: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    var color = Color(red: 0.2, green: 0.2, blue: 0.2)
+}
+
+struct CalculatorView: View {
+    let spacing: CGFloat = 12
+    let buttons: [[CalculatorButton]] = [
+        [.init(title: "AC", color: .lightGray),
+         .init(title: "±", color: .lightGray),
+         .init(title: "%", color: .lightGray),
+         .init(title: "÷", color: Color.orange)],
+        [.init(title: "7"),
+         .init(title: "8"),
+         .init(title: "9"),
+         .init(title: "x", color: Color.orange)],
+        [.init(title: "4"),
+         .init(title: "5"),
+         .init(title: "6"),
+         .init(title: "-", color: Color.orange) ],
+        [.init(title: "1"),
+         .init(title: "2"),
+         .init(title: "3"),
+         .init(title: "+", color: Color.orange)],
+        [.init(title: "0"),
+        .init(title: "."),
+        .init(title: "=", color: Color.orange)]
+    ]
+    
     var body: some View {
-        Text("Hello World")
+        GeometryReader { geometry in
+            ZStack {
+                Color.black.edgesIgnoringSafeArea(.all)
+                VStack(spacing: self.spacing) {
+                    Spacer()
+                    ForEach(self.buttons, id: \.self) { row in
+                        CalculatorButtonsRow(screenWidth: geometry.size.width, spacing: self.spacing, buttons: row)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct CalculatorButtonsRow: View {
+    let screenWidth: CGFloat
+    let spacing: CGFloat
+    let buttons: [CalculatorButton]
+    
+    func getButtonWidth(title: String) -> CGFloat {
+        return title != "0" ?
+        (self.screenWidth - self.spacing * 5) / 4 :
+        (self.screenWidth - self.spacing * 5) / 4 * 2 + self.spacing
+    }
+    
+    var body: some View {
+        HStack (spacing: self.spacing) {
+            Spacer()
+            ForEach(self.buttons) { button in
+                Text(button.title)
+                    .font(.system(size: 28))
+                    .foregroundColor(.white)
+                    .frame(width: self.getButtonWidth(title: button.title), height: (self.screenWidth - self.spacing * 5) / 4)
+                    .background(button.color)
+                    .cornerRadius(100)
+            }
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        CalculatorView()
     }
 }
